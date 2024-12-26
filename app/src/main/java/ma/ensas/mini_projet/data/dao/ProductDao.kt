5587bdff0b5ff55f26bb4fb8338afc6f3ab3bccb
Mustapha Addazi <addazimustapha123@gmail.com>
2024-12-26 10:24:25 +0100

    no changes

--- app/src/main/java/ma/ensas/mini_projet/data/dao/ProductDao.kt
+++ app/src/main/java/ma/ensas/mini_projet/data/dao/ProductDao.kt
@@ -24,7 +24,6 @@ interface ProductDao {
     @Transaction
     @Query("SELECT * FROM products")
     suspend fun getAllProducts(): List<Product>
-    // Méthode pour supprimer tous les produits
     @Query("DELETE FROM products")
     suspend fun deleteAllProducts()
     @Query("SELECT * FROM products WHERE productId = :productId LIMIT 1")
