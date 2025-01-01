1916ae9edf974e8a848afad5f9e81857e2ab8913
belharche <brahimelharche02@gmail.com>
2025-01-01 22:30:57 +0100

    stable version (reservation to be fixed)

---
+++ app/src/main/java/ma/ensas/mini_projet/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,8 @@
+package ma.ensas.mini_projet.ui.dashboard
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class DashboardViewModel(app : Application) : AndroidViewModel(app) {
+    // TODO: Implement the ViewModel
+}
